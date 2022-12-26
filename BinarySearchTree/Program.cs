d72bcea73dde37f3fd15db1b95bf331bcfdbd412
Ardian Mulya <ardian.mulya.ft21@mail.umy.ac.id>
2022-12-26 17:46:12 +0700

    membuat tampilan menu

--- BinarySearchTree/Program.cs
+++ BinarySearchTree/Program.cs
@@ -113,7 +113,18 @@ namespace BinarySearchTree
         }
         static void Main(string[] args)
         {
-            BinaryTree x = new BinaryTree(); 
+            BinaryTree x = new BinaryTree();
+            while (true)
+            {
+                Console.WriteLine("\nMenu");
+                Console.WriteLine("1. Implement insert operation");
+                Console.WriteLine("2. Perform inorder traversal");
+                Console.WriteLine("3. Perform preorder traversal");
+                Console.WriteLine("4. Perform postorder traversal");
+                Console.WriteLine("5. Exit\n");
+                Console.WriteLine("Enter your choice (1-5): ");
+                char ch = Convert.ToChar(Console.ReadLine());
+            }
         }
     }
 }
